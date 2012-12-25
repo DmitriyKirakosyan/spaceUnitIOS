f23cd313df153d379d2298a3647a8a141741204a
Dima <dmitriy.kirakosyan@gmail.com>
2012-12-25 20:54:20 +0400

    init

---
+++ spaceUnit/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  spaceUnitAppController.h
+//  spaceUnit
+//
+//  Created by Dmitriy on 12/25/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ spaceUnit/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  spaceUnitAppController.h
+//  spaceUnit
+//
+//  Created by Dmitriy on 12/25/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
